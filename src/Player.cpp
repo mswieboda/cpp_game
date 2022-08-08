573b5b97b504ca2ceeb530841c053e34d8c16824
Matt Swieboda <mswieboda@users.noreply.github.com>
2022-08-07 22:21:26 -0500

    adds player idle_walk_left animation

--- src/Player.cpp
+++ src/Player.cpp
@@ -10,33 +10,47 @@ Player::Player(): animations() {
 void Player::initAnimations(ALLEGRO_BITMAP* sheet) {
   Animation idle(FPS / 3, true, true);
 
-  const int frames = 5;
   const int size = 64;
+  int frames = 5;
+  int i = 0;
 
-  for (int i = 0; i < frames; i++) {
+  for (; i < frames; i++)
     idle.add(sheet, i * size, 0, size, size);
-  }
+
+  Animation idle_walk_left(FPS / 3, true, true);
+
+  frames += 4;
+
+  for (; i < frames; i++)
+    idle_walk_left.add(sheet, i * size, 0, size, size);
 
   animations.add(idle, "idle");
+  animations.add(idle_walk_left, "idle_walk_left");
 
   animations.play("idle");
 }
 
 void Player::update(Keys keys) {
+  int dx = 0;
+  int dy = 0;
+
   if (keys.anyPressed(2, ALLEGRO_KEY_UP, ALLEGRO_KEY_W))
-    y -= speed;
+    dy -= speed;
   if (keys.anyPressed(2, ALLEGRO_KEY_DOWN, ALLEGRO_KEY_S))
-    y += speed;
-  if (keys.anyPressed(2, ALLEGRO_KEY_LEFT, ALLEGRO_KEY_A))
-    x -= speed;
+    dy += speed;
+  if (keys.anyPressed(2, ALLEGRO_KEY_LEFT, ALLEGRO_KEY_A)) {
+    dx -= speed;
+    animations.play("idle_walk_left");
+  }
   if (keys.anyPressed(2, ALLEGRO_KEY_RIGHT, ALLEGRO_KEY_D))
-    x += speed;
+    dx += speed;
 
-  if (keys.isJustPressed(ALLEGRO_KEY_1))
+  if (dx == 0 && dy == 0)
     animations.play("idle");
-
-  if (keys.isJustPressed(ALLEGRO_KEY_2))
-    animations.play("explosion");
+  else {
+    x += dx;
+    y += dy;
+  }
 
   animations.update();
 }
